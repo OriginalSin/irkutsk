b879b21b601c7787c96df2f9c3b579bed0eb9722
Serg <serg@auchat.ru>
2017-12-22 20:02:34 +0300

    slope

--- js/energomera.js
+++ js/energomera.js
@@ -8,27 +8,16 @@ var map = new L.Map(document.body.getElementsByClassName('map')[0], {
 	zoom: 10
 });
 L.gmx.loadMap('F4BEA507CF0A416081C21300AF0AD68A', {leafletMap: map});
-var blm = map.gmxBaseLayersManager;
-var sat1 = L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA', {zIndex: -1000000}),
-	slope = L.tileLayer.Mercator('http://{s}.tile.cart.kosmosnimki.ru/ds/{z}/{x}/{y}.png');
-// blm.add('slope', {
-	// layers: [slope]
-// });
-// blm.add('satellite', {
-	// layers: [sat1]
-// });
-// blm.setActiveIDs(['slope', 'satellite']);
-// blm.setCurrentID('satellite');
 L.control.iconLayers([
 	{
 		title: 'Снимки',
 		icon: '//maps.kosmosnimki.ru/api/img/baseLayers/basemap_satellite.png',
-		layer: sat1
+		layer: L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA', {zIndex: -1000000})
 	},
 	{
 		title: 'Уклоны',
 		icon: '//maps.kosmosnimki.ru/api/img/baseLayers/basemap_relief_slope.png',
 		description: '<img src = "//maps.kosmosnimki.ru/api/img/baseLayers/basemap_relief_slope_legend.svg"></img>',
-		layer: slope
+		layer: L.tileLayer.Mercator('http://{s}.tile.cart.kosmosnimki.ru/ds/{z}/{x}/{y}.png', {zIndex: -1000000})
 	}
 ], {}).addTo(map);
