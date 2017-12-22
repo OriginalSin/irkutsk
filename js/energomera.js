42aea9e73a877afa650754d699bc429b952ac81e
Serg <serg@auchat.ru>
2017-12-22 19:43:37 +0300

    iconLayers

--- js/energomera.js
+++ js/energomera.js
@@ -9,38 +9,27 @@ var map = new L.Map(document.body.getElementsByClassName('map')[0], {
 });
 L.gmx.loadMap('F4BEA507CF0A416081C21300AF0AD68A', {leafletMap: map});
 var blm = map.gmxBaseLayersManager;
+var sat1 = L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA', {zIndex: -1000000}),
+	slope = L.tileLayer.Mercator('//{s}.tilecart.kosmosnimki.ru/ds/{z}/{x}/{y}.png');
 blm.add('slope', {
-	layers: [
-        L.tileLayer.Mercator('//{s}.tilecart.kosmosnimki.ru/ds/{z}/{x}/{y}.png')
-	]
+	layers: [slope]
 });
 blm.add('satellite', {
-	layers: [
-		L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA', {zIndex: -1000000})
-	]
+	layers: [sat1]
 });
 blm.setActiveIDs(['slope', 'satellite']);
 blm.setCurrentID('satellite');
-L.control.iconLayers().addTo(map);
-/*
-blm.initDefaults({srs: 3857}).then(function() {
-	var baseLayers = blm.getAll().map(function(baseLayer) { return baseLayer.id; });
-	var satellite = blm.get('satellite'),
-		sat1 = L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA', {zIndex: -1000000}),
-		slope = blm.get('slope');
-	L.control.iconLayers([
-		{
-			title: satellite.options.rus,
-			icon: satellite.options.icon,
-			layer: sat1
-		},
-		{
-			title: slope.options.rus,
-			icon: slope.options.icon,
-			layer: slope
-		}
-	], {}).addTo(map);
-	//blm.setActiveIDs(baseLayers).setCurrentID('satellite');
-
-});
-*/
+var satellite = blm.get('satellite'),
+	slope = blm.get('slope');
+L.control.iconLayers([
+	{
+		title: satellite.options.rus,
+		icon: satellite.options.icon,
+		layer: sat1
+	},
+	{
+		title: slope.options.rus,
+		icon: slope.options.icon,
+		layer: slope
+	}
+], {}).addTo(map);
