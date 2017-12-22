f08eae1d145d01f3db3b79e2bb78a9e77e9aefcf
Serg <serg@auchat.ru>
2017-12-22 19:23:14 +0300

    zIndex

--- js/energomera.js
+++ js/energomera.js
@@ -2,17 +2,17 @@ var map = new L.Map(document.body.getElementsByClassName('map')[0], {
 	layers: [],
 	attributionControl: false,
 	zoomControl: true,
-	srs: 3857,
+	//srs: 3857,
 	scrollWheelZoom: true,
 	center: new L.LatLng(45.47987347203981, 42.03712463378906),
 	zoom: 10
 });
-L.gmx.loadMap('F4BEA507CF0A416081C21300AF0AD68A', {leafletMap: map, srs: 3857});
+L.gmx.loadMap('F4BEA507CF0A416081C21300AF0AD68A', {leafletMap: map});
 var blm = map.gmxBaseLayersManager;
 blm.initDefaults({srs: 3857}).then(function() {
 	var baseLayers = blm.getAll().map(function(baseLayer) { return baseLayer.id; });
 	var satellite = blm.get('satellite'),
-		sat1 = L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA'),
+		sat1 = L.tileLayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&srs=3857&apikey=AYU65737MA', {zIndex: -1000000}),
 		slope = blm.get('slope');
 	L.control.iconLayers([
 		{
