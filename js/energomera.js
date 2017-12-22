e34f72fab83b748b6acc4bd201407b4ed752e3dd
Serg <serg@auchat.ru>
2017-12-22 19:11:54 +0300

    sat1

--- js/energomera.js
+++ js/energomera.js
@@ -2,21 +2,23 @@ var map = new L.Map(document.body.getElementsByClassName('map')[0], {
 	layers: [],
 	attributionControl: false,
 	zoomControl: true,
+	srs: 3857,
 	scrollWheelZoom: true,
 	center: new L.LatLng(45.47987347203981, 42.03712463378906),
 	zoom: 10
 });
 L.gmx.loadMap('F4BEA507CF0A416081C21300AF0AD68A', {leafletMap: map, srs: 3857});
 var blm = map.gmxBaseLayersManager;
-blm.initDefaults().then(function() {
+blm.initDefaults({srs: 3857}).then(function() {
 	var baseLayers = blm.getAll().map(function(baseLayer) { return baseLayer.id; });
 	var satellite = blm.get('satellite'),
+		sat1 = L.tilelayer('//maps.kosmosnimki.ru/TileService.ashx?request=getTile&map=PLDYO&LayerName=63E083C0916F4414A2F6B78242F56CA6&z={z}&x={x}&y={y}&ftc=osm&srs=3857&apikey=AYU65737MA'),
 		slope = blm.get('slope');
 	L.control.iconLayers([
 		{
 			title: satellite.options.rus,
 			icon: satellite.options.icon,
-			layer: satellite
+			layer: sat1
 		},
 		{
 			title: slope.options.rus,
